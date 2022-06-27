b8f95c7f500ec402e56c35f5fe5f0e513e39576f
sheikhjaber0424 <55077645+sheikhjaber0424@users.noreply.github.com>
2022-06-27 11:33:16 -0700

    add & delete task done

---
+++ src/components/TodoApp.vue
@@ -0,0 +1,111 @@
+<template>
+<div class="container">
+<h2 class="text-center mt-5">My Vue Todo-List</h2>
+    <div class="d-flex">
+        <input v-model="addTask" type="text" placeholder="Enter task" class="form-control">
+        <button  @click="submitTask" class="btn btn-warning rounded-0">SUBMIT</button>
+    </div>
+
+
+    <table class="table table-bordered">
+  <thead>
+    <tr>
+      <th scope="col">Task</th>
+      <th scope="col">Status</th>
+      <th scope="col" class="text-center">#</th>
+      <th scope="col" class="text-center">#</th>
+    </tr>
+  </thead>
+  <tbody>
+
+    <tr v-for="(task,index) in tasks" :key="index">
+
+      <td >{{ task.name }}</td>
+      <td>{{ task.status }}</td>
+      <td>
+        <div @click="taskEdit(index)" class="text-center">
+       <span> <fa icon="pen"/> </span>
+      </div>
+      </td>
+      <td>
+        <div @click="deleteTask(index)" class="text-center" >
+       <span> <fa icon="trash"/> </span>
+      </div>
+      </td>
+      
+    </tr>
+
+  </tbody>
+</table>
+
+
+
+
+</div>
+  
+</template>
+
+<script>
+
+export default {
+  data(){
+    return{
+
+
+      addTask: '',
+      editTaskIndex: null,
+      tasks: [
+        {
+            name :'Cycling',
+            status : 'to-do'
+        },
+        {
+            name :'Study',
+            status : 'to-do'
+        },
+        {
+            name :'Shopping',
+            status : 'to-do'
+        }
+        
+      ]
+    }
+  },
+  methods: {
+    submitTask(){
+
+   console.log(this.editTaskIndex)
+
+   
+      if(this.addTask.length!=0 )
+      {
+        this.tasks.push({
+          name: this.addTask,
+          status :'to-do',
+          
+        })
+        console.log('taskadded')
+      }
+  
+      
+     
+    
+    
+    
+      this.addTask = ''
+      console.log('cleared outside')
+    },
+
+    deleteTask(index){
+      this.tasks.splice(index,1)
+    },
+
+
+  }
+
+}
+</script>
+
+<style>
+
+</style>
--- src/main.js
+++ src/main.js
@@ -1,4 +1,14 @@
 import { createApp } from 'vue'
-import App from './App.vue'
+import "bootstrap/dist/css/bootstrap.min.css" ;
 
-createApp(App).mount('#app')
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+import App from './App.vue';
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { fas } from '@fortawesome/free-solid-svg-icons';
+
+library.add(fas)
+
+
+createApp(App)
+.component('fa',FontAwesomeIcon)
+.mount('#app')
